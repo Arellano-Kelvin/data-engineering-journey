ef9eb4ddef189432c84ae25aa96cbd0122babf86
Arellano-Kelvin <arellano.kelvin.91@gmail.com>
2026-02-23 06:10:28 -0700

    restarting day 1

--- month1/week1/week1_day1_variables.py
+++ month1/week1/week1_day1_variables.py
@@ -1,1 +1,106 @@
 # Variables and basic assignment
+spam = 40
+spam
+eggs=2
+spam+eggs
+spam+eggs+spam
+spam=spam+2
+spam
+spam = 'Hello'
+spam
+spam='Goodbye'
+spam
+print('Hello, world!')
+print('What is your name?')
+my_name=input('>')
+print('It is good to meet you, '+my_name)
+print('The length of your name is: ')
+print(len(my_name))
+print('What is your age?')
+my_age=input('>')
+print('You will be ' + str(int(my_age) + 1) + ' in a year.')
+len('hello')
+len('My very energetic monster just scarfed nachos')
+len('')
+print('I am ' +29+ ' years old.')
+str(29)
+print('I am '+str(29)+' years old.')
+str(0)
+str(-3.14)
+int('42')
+int('-99')
+int(1.25)
+int(1.99)
+float('3.14')
+float(10)
+spam=input('>')
+spam
+spam=int(spam)
+spam*10/5
+int('99.99')
+int('twelve')
+int(7.7)
+int(7.7)+1
+print('What is your age?') #Ask for their age.
+my_age=input('>')
+print('You will be '+str(int(my_age)+1)+' in a year.')
+42=='42'
+42==42.0
+42.0==0042.00
+type(42)
+type(42.0)
+type('forty two')
+name = 'Zophie'
+type(name)
+type(len(name))
+round(3.14)
+round(7.7)
+round(-2.2)
+round(3.14,1)
+round(7.7777,3)
+abs(25)
+abs(-25)
+abs(-3.14)
+abs(0)
+'''
+Which of the following are operators, and which are values?
+
+*
+'hello'
+-88.8
+-
+/
++
+5
+
+Which of the following is a variable, and which is a string?
+
+spam
+'spam'
+
+Name three data types.
+
+What is an expression made up of? What do all expressions do?
+
+This chapter introduced assignment statements, like spam = 10. What is the difference between an expression and a statement?
+
+What does the variable bacon contain after the following code runs?
+
+bacon = 20
+bacon + 1
+
+What should the following two expressions evaluate to?
+
+'spam' + 'spamspam'
+'spam' * 3
+Why is eggs a valid variable name while 100 is invalid?
+
+What three functions can be used to get the integer, floating-point number, or string version of a value?
+
+Why does this expression cause an error? How can you fix it?
+
+'I eat ' + 99 + ' burritos.'
+Extra credit: Search online for the Python documentation for the len() function. 
+It will be on a web page titled “Built-in Functions.” 
+Skim the list of other functions Python has, look up what the bin() and hex() functions do, and experiment with them in the interactive shell.
+'''
